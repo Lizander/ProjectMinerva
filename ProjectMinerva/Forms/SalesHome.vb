a64fce47a80f837d5a2038edb1cd1b5c8bf3d265
Kevin Lizander <kevin9075@gmail.com>
2016-11-25 18:32:04 -0400

    Fix navigation errors in FindCustomers form

--- ProjectMinerva/Forms/SalesHome.vb
+++ ProjectMinerva/Forms/SalesHome.vb
@@ -5,8 +5,6 @@
     Private Sub LineItemsBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs)
         Me.Validate()
         Me.LineItemsBindingSource.EndEdit()
-
-
     End Sub
 
     Private Sub SalesHome_Load(sender As Object, e As EventArgs) Handles MyBase.Load
