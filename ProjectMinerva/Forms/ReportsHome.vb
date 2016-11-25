f351c33bb32ee29603d5d87e4902579a570ead24
Kevin Lizander <kevin9075@gmail.com>
2016-11-25 19:12:19 -0400

    Add ReportsHome form

---
+++ ProjectMinerva/Forms/ReportsHome.vb
@@ -0,0 +1,7 @@
+﻿Public Class ReportsHome
+
+    Private Sub BackButton_Click(sender As Object, e As EventArgs) Handles BackButton.Click
+        AdministrationPanel.Show()
+        Me.Close()
+    End Sub
+End Class
